a68e5a398c222651f2673385f6cc9b49188cb40d
jimmyn123 <jimmyn123@gmail.com>
2018-10-08 11:47:28 -0700

    fixed file structure

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace Calculator
 {
@@ -6,7 +7,45 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool valid;
+            do
+            {
+                WriteLine("Choose an operation 1) Addition 2) Subtraction 3) Division 4) Multiplication: ");
+                int selection;
+                valid = int.TryParse(ReadLine(), out selection);
+            } while (!valid);
+
+            WriteLine("Insert first operand:");
+            decimal x;
+            decimal.TryParse(ReadLine(), out x);
+        }
+
+        static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        static decimal Subtract(decimal x, decimal y)
+        {
+            return x - y;
+        }
+
+        static decimal Multiply(decimal x, decimal y)
+        {
+            return x * y;
+        }
+
+        static decimal Divide(decimal x, decimal y)
+        {
+            try
+            {
+                return x / y;
+            }
+            catch (DivideByZeroException ex)
+            {
+                WriteLine("Can't divide by 0!");
+                return 0;
+            }
         }
     }
 }
